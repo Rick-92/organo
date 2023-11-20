31beced905d7360101166f9d8a98fae5aa8baf95
Rick-92 <k.abc.1@hotmail.com>
2023-11-19 22:58:40 -0300

    projeto1

---
+++ src/componentes/banner/index.js
@@ -0,0 +1,12 @@
+import "./index.css"
+
+function Banner() {
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="banner principal" />
+        </header>
+
+    )
+}
+
+export default Banner
